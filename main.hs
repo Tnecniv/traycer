b0674368506473f2f784e53c4e4c0ea2fec52164
Vincent Pacelli <vspacelli@gmail.com>
2015-12-26 14:19:20 -0500

    Added reflections

--- main.hs
+++ main.hs
@@ -6,8 +6,7 @@ import qualified Data.ByteString.Lazy as BL
 import System.IO
 import Data.Array
 
-type R = Float
-data Vec = Vec { xval :: Float, yval :: R, zval :: R } deriving (Eq, Show)
+data Vec = Vec { xval :: Float, yval :: Float, zval :: Float } deriving (Eq, Show)
 
 instance Num Vec where
     (Vec a1 b1 c1) + (Vec a2 b2 c2) = Vec (a1 + a2) (b1 + b2) (c1 + c2)
@@ -18,23 +17,23 @@ instance Num Vec where
     signum = undefined
     fromInteger n = (Vec nf nf nf) where nf = fromInteger n
  
-dot :: Vec -> Vec -> R
+dot :: Vec -> Vec -> Float
 dot (Vec a1 b1 c1) (Vec a2 b2 c2) = (a1 * a2) + (b1 * b2) + (c1 * c2)
 
-scalarMult :: R -> Vec -> Vec
+scalarMult :: Float -> Vec -> Vec
 scalarMult alpha (Vec a b c) = Vec (alpha * a) (alpha * b) (alpha * c)
 
-norm :: Vec -> R
+norm :: Vec -> Float
 norm a = sqrt $ dot a a
 
 normalize :: Vec -> Vec
 normalize (Vec a b c) = Vec (a / n) (b / n) (c / n) where n = norm (Vec a b c)
 
 -- sqrt not necessary for comparisons
-distSqr :: Vec -> Vec -> R
+distSqr :: Vec -> Vec -> Float
 distSqr a b = norm $ a - b
 
-dist :: Vec -> Vec -> R
+dist :: Vec -> Vec -> Float
 dist a b = sqrt $ dist a b
 
 -- I didn't make this signum because i can't guarantee (abs a) * (signum a) = a
@@ -42,8 +41,8 @@ dist a b = sqrt $ dist a b
 signs :: Vec -> Vec
 signs (Vec a b c) = Vec (signum a) (signum b) (signum c)
 
-data Material = Material { matColor :: Color, matPower :: Float, matDiffuse :: Float, matSpecular :: Float } deriving (Eq, Show)
-data Sphere = Sphere { sphereOrigin :: Vec, sphereRadius :: R } deriving (Eq, Show)
+data Material = Material { matColor :: Color, matPower :: Float, matDiffuse :: Float, matSpecular :: Float, matReflectivity :: Float } deriving (Eq, Show)
+data Sphere = Sphere { sphereOrigin :: Vec, sphereRadius :: Float } deriving (Eq, Show)
 
 data Ray = Ray { rayOrigin :: Vec, rayDirection :: Vec } deriving (Eq, Show) -- Note: Direction is normalized
 
@@ -97,7 +96,7 @@ pointOfIntersection r (Just vecs) =
         minElem = justOrZero (elemIndex m d) in
             vecs !! minElem
 
-distToIntersectionSqr :: Ray -> Maybe [Vec] -> R
+distToIntersectionSqr :: Ray -> Maybe [Vec] -> Float
 distToIntersectionSqr r int = case int of Nothing -> infinity
                                           Just vecs -> let d = map (distSqr $ rayOrigin r) vecs
                                                            m = minimum d in
@@ -127,27 +126,40 @@ pointIsInShadow point scene (x:xs) =
 
 pointIsInShadow _ _ [] = False
 
-phong :: Scene -> Ray -> Maybe Ray -> Material -> [Light] -> Color
-phong scene ray normal mat lights =
+reflectRay :: Ray -> Ray -> Ray
+reflectRay (Ray incPos incDir) (Ray normPos normDir) = Ray (incPos + (scalarMult epsilon reflDir)) reflDir
+    where reflDir = (scalarMult (-2 * (dot incDir normDir)) normDir) + incDir
+          epsilon = 0.001
+
+shade :: Scene -> Ray -> Maybe Ray -> Material -> [Light] -> Int -> Color
+shade scene ray normal mat lights reflectionTreeDepth =
     case normal of Just normRay -> let diff = (matDiffuse mat) `scalarMult` (diffuse normRay mat lights)
                                        spec = (matSpecular mat) `scalarMult` (specular ray normRay mat lights)
-                                       shadow = pointIsInShadow (rayOrigin normRay) scene lights in
+                                       shadow = pointIsInShadow (rayOrigin normRay) scene lights
+                                       reflection = traceRay scene (reflectRay ray normRay) lights (reflectionTreeDepth - 1)
+                                       reflColor = scalarMult (matReflectivity mat) reflection in
+                                       if matReflectivity mat == 0 || reflectionTreeDepth == 0 then
                                            if shadow == True then
                                                ambient
                                            else
                                                ambient + diff + spec
-                   Nothing -> Vec 0 0 0 -- I'm going to punt on edges for now. On polyhedra, can probably just pick any normal for edges? Investigate
+                                       else
+                                           if shadow == True then
+                                               ambient + reflColor
+                                           else
+                                               ambient + diff + spec + reflColor
+                   Nothing -> ambient -- I'm going to punt on edges for now. On polyhedra, can probably just pick any normal for edges? Investigate
     where ambient = 0.1 `scalarMult` matColor mat
 
 diffuse :: Ray -> Material -> [Light] -> Color
-diffuse (Ray normOri normDir) (Material mc _ _ _) lights = sum colors
+diffuse (Ray normOri normDir) (Material mc _ _ _ _) lights = sum colors
     where lightDirs = map (\x -> normalize $ x - normOri) lights
           lamberts = map (\x -> max (dot normDir x) 0) lightDirs -- if dot is negative, light is behind face.
           colors = map (\x -> scalarMult x mc) lamberts
 
 -- Blinn-Torrance model
 specular :: Ray -> Ray -> Material -> [Light] -> Color
-specular (Ray viewOri viewDir) (Ray normOri normDir) (Material _ power _ _) lights = sum colors
+specular (Ray viewOri viewDir) (Ray normOri normDir) (Material _ power _ _ _) lights = sum colors
     where backwardViewDir = scalarMult (-1) viewDir
           lightDirs = map (\x -> normalize $ x - normOri) lights
           bisections = map (\x -> normalize (x + backwardViewDir)) lightDirs
@@ -171,10 +183,10 @@ castRays (Camera pos look v dist) width height xPixels yPixels pixelDensity = ma
 vec2pix :: Vec -> PixelRGBF
 vec2pix (Vec a b c) = PixelRGBF a b c
 
-traceRay :: Scene -> Ray -> [Light] -> PixelRGBF
-traceRay scene ray lights =
-    case closest of Just (vec, normFunc, mat) -> vec2pix $ phong scene ray (normFunc vec) mat lights 
-                    Nothing -> PixelRGBF 0 0 0
+traceRay :: Scene -> Ray -> [Light] -> Int -> Color
+traceRay scene ray lights reflectionTreeDepth =
+    case closest of Just (vec, normFunc, mat) -> shade scene ray (normFunc vec) mat lights reflectionTreeDepth
+                    Nothing -> Vec 0 0 0
     where closest = closestIntersection scene ray
    
 main = do
@@ -185,14 +197,19 @@ main = do
           pixelDensity = 100000.0
           pixelWidth = floor $ (sqrt pixelDensity) * planeWidth
           pixelHeight = floor $ (sqrt pixelDensity) * planeHeight
-          sphere1 = Sphere (Vec 1 0 2) 1
-          sphere2 = Sphere (Vec (-1) 0 2) 1
-          sphere3 = Sphere (Vec 0 (-401) 2) 400
-          scene = [(intersectsRay sphere1, normalAtPoint sphere1, (Material (Vec (135/255) (67/255) (232/255)) 10 1 1)),
-                   (intersectsRay sphere2, normalAtPoint sphere2, (Material (Vec (255/255) (0/255) (0/255)) 10 1 1)),
-                   (intersectsRay sphere3, normalAtPoint sphere3, (Material (Vec (13/255) (181/255) (255/255)) 10 1 1))]
-          lights = [Vec 0 3 0]
+          sphere1 = Sphere (Vec 0 1 (-3)) 1
+          sphere2 = Sphere (Vec (0) 2 6) 4
+          sphere3 = Sphere (Vec (-1) 0 (-3)) 1
+          sphere4 = Sphere (Vec (1) 0 (-3)) 1 
+          sphere5 = Sphere (Vec 0 (-401) 2) 400
+          scene = [(intersectsRay sphere1, normalAtPoint sphere1, (Material (Vec (135/255) (67/255) (232/255)) 10 1 1 0)),
+                   (intersectsRay sphere2, normalAtPoint sphere2, (Material (Vec (255/255) (255/255) (255/255)) 50 1 1 1)),
+                   (intersectsRay sphere3, normalAtPoint sphere3, (Material (Vec (255/255) (0/255) (0/255)) 10 1 1 0)),
+                   (intersectsRay sphere4, normalAtPoint sphere4, (Material (Vec (0/255) (255/255) (0/255)) 10 1 1 0)),
+                   (intersectsRay sphere5, normalAtPoint sphere5, (Material (Vec (13/255) (181/255) (255/255)) 10 1 1 0))]
+          lights = [Vec 0 10 0]
           rays = castRays cam planeWidth planeHeight pixelWidth pixelHeight pixelDensity
-          pixels = map (\x -> traceRay scene x lights) rays
+          colors = map (\x -> traceRay scene x lights 4) rays
+          pixels = map vec2pix colors
           imageArray = listArray ((0, 0), (pixelWidth - 1, pixelHeight - 1)) pixels
           img = ImageRGBF $ generateImage (\x y -> imageArray ! (x,y)) pixelWidth pixelHeight
